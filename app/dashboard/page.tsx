af07c218f6cce7985963849fbf3b670b6937fa35
ogechimiracle <ogechiiheaka@gmail.com>
2026-04-12 21:27:02 +0100

    fixed user dashboard issues

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { User } from "lucide-react"
+
+function UserDasboard(){
+    return(
+        <div>
+            <h1 className="text-2xl font-bold">User Dashboard</h1>
+            <p>Welcome to your dashboard! Here you can view your courses, progress, and more.</p>
+        </div>
+    )
+}
+
+export default UserDasboard;
