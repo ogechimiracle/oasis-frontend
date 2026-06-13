21a80401b1cb697732d608d6e22656048d392113
ogechimiracle <ogechiiheaka@gmail.com>
2026-06-13 16:40:33 +0100

    added chat support

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import { Toaster } from "react-hot-toast";
 import ScrollToTop from "@/components/scrollTo";
+import Script from "next/script";
 
 
 
@@ -16,15 +17,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
-        <div>
-          <ScrollToTop/>
-          <Nav />
-          {children}
-          <Footer />
-          <Toaster/>
-        </div>
-        
-     
+    <div>
+      <ScrollToTop />
+      <Nav />
+      {children}
+      <Footer />
+      <Toaster />
+
+      <Script id="chaport-config" strategy="afterInteractive">
+        {`
+          (function(w,d,v3){
+            w.chaportConfig = {
+              appId: '6a2d7789e5e5ce985ae60c61',
+            };
+
+            if(w.chaport)return;
+            v3=w.chaport={};
+            v3._q=[];
+            v3._l={};
+            v3.q=function(){v3._q.push(arguments)};
+            v3.on=function(e,fn){
+              if(!v3._l[e])v3._l[e]=[];
+              v3._l[e].push(fn);
+            };
+
+            var s=d.createElement('script');
+            s.type='text/javascript';
+            s.async=true;
+            s.src='https://app.chaport.com/javascripts/insert.js';
+
+            var ss=d.getElementsByTagName('script')[0];
+            ss.parentNode.insertBefore(s,ss);
+          })(window, document);
+        `}
+      </Script>
+    </div>
   );
 }
